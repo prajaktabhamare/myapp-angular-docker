249c8fda5e80de50abea3e33150203b35a645bf4
Prajkta <prajaktabhamare120@gmail.com>
2019-12-03 15:11:00 +0530

    initial app

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products = []
+  constructor(private service : ProductService) { }
+
+  ngOnInit()
+   {
+     console.log("hiii");
+     this.service.getProducts().subscribe(Response =>{
+       if(Response['status']==='success'){
+         this.products = Response['data'] as any[]
+
+       }
+       else{
+         console.log('error',Response['error'])
+       }
+     })
+  }
+
+}
---
+++ src/app/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  url = 'http://172.17.0.1:4444/product'
+  constructor(private http:HttpClient)
+   { 
+
+   }
+
+   getProducts()
+   {
+     return this.http.get(this.url)
+   }
+}
